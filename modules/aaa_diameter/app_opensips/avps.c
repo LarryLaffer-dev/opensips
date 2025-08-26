398735f680d07a4a29b900fa2c55cddf3b6f5e73
Carsten <larrylaffer130@gmail.com>
2025-08-26 10:10:42 +0200

    Fix memlead in command parsing

--- modules/aaa_diameter/app_opensips/avps.c
+++ modules/aaa_diameter/app_opensips/avps.c
@@ -885,7 +885,9 @@ int parse_command_def(char *line, FILE *fp, int cmd_type)
 	unsigned int cmd_code = -1;
 	char *p = line, cmd_name[128 + 1], *bkp, *newp;
 	size_t buflen = strlen(line);
-	int i, len = buflen, cmd_name_len = -1, avp_count = 0;
+	size_t len = buflen;
+	ssize_t read;
+	int i, cmd_name_len = -1, avp_count = 0;
 	struct dm_avp_def avps[128];
 
 	switch (cmd_type) {
@@ -934,14 +936,17 @@ int parse_command_def(char *line, FILE *fp, int cmd_type)
 
 	LOG_DBG("parsed Cmd-Code %d (%s)\n", cmd_code, cmd_name);
 
-	while (getline(&line, &buflen, fp) >= 0) {
+	free(line);
+	line = NULL;
+	len = 0;
+	while ((read = getline(&line, &buflen, fp)) >= 0) {
 		p = line;
 		len = strlen(p);
 
 		while (isspace(*p)) { p++; len--; }
 
 		if (*p == '{')
-			continue;
+			goto next;
 
 		if (*p == '}' || !strlen(p))
 			goto define_req;
@@ -955,6 +960,10 @@ int parse_command_def(char *line, FILE *fp, int cmd_type)
 			LOG_ERROR("failed to parse Command AVP line: '%s'\n", line);
 			return -1;
 		}
+next:
+		free(line);
+		line = NULL;
+		len = 0;
 	}
 
 define_req:
@@ -1053,14 +1062,14 @@ int parse_extra_avps(const char *extra_avps_file)
 
 		// comment or empty line
 		if (*p == '#' || p - line >= read)
-			continue;
+			goto next;
 
 		rc = parse_app_vendor(p, fp);
 		if (rc < 0) {
 			ret = -1;
 			goto out;
 		} else if (rc == 0) {
-			continue;
+			goto next;
 		}
 
 		rc = parse_attr_def(p, fp);
@@ -1068,7 +1077,7 @@ int parse_extra_avps(const char *extra_avps_file)
 			ret = -1;
 			goto out;
 		} else if (rc == 0) {
-			continue;
+			goto next;
 		}
 
 		rc = parse_app_def(p, fp);
@@ -1076,7 +1085,7 @@ int parse_extra_avps(const char *extra_avps_file)
 			ret = -1;
 			goto out;
 		} else if (rc == 0) {
-			continue;
+			goto next;
 		}
 
 		rc = parse_command_def(p, fp, CMD_REQUEST);
@@ -1085,7 +1094,7 @@ int parse_extra_avps(const char *extra_avps_file)
 			goto out;
 		} else if (rc == 0) {
 			answers_needed++;
-			continue;
+			goto next;
 		}
 
 		rc = parse_command_def(p, fp, CMD_ANSWER);
@@ -1096,6 +1105,11 @@ int parse_extra_avps(const char *extra_avps_file)
 			answers_needed--;
 			continue;
 		}
+
+	next:
+		free(line);
+		line = NULL;
+		len = 0;
 
 		// unknown line... ignoring
 	}
@@ -1108,7 +1122,6 @@ int parse_extra_avps(const char *extra_avps_file)
 
 out:
 	fclose(fp);
-	free(line);
 
 	return ret;
 }
